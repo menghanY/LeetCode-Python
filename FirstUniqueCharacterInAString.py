e1675779c6bbf6ce469e38140d75f03133802cec
menghan <menghan_yan@foxmail.com>
2016-11-09 11:10:21 +0800

    Simplified get method

--- FirstUniqueCharacterInAString.py
+++ FirstUniqueCharacterInAString.py
@@ -2,8 +2,7 @@ class Solution(object):
     def firstUniqChar(self, s):
         numDict = {}
         for charS in s:
-            if numDict.get(charS) : numDict[charS] += 1
-            else :              numDict[charS] = 1
+            numDict[charS] = numDict.get(charS,0) + 1
         num = 0
         for charS in s:
             if numDict.get(charS) == 1 :
--- ValidAnagram.py
+++ ValidAnagram.py
@@ -5,12 +5,10 @@ class Solution(object):
         tDict = {}
         for xChar in s :
             if xChar == "" : continue
-            if sDict.get(xChar) : sDict[xChar] += 1
-            else :               sDict[xChar] = 1
+            sDict[xChar] = sDict.get(xChar,0) + 1
         for xChar in t :
             if xChar == "" : continue
-            if tDict.get(xChar) : tDict[xChar] += 1
-            else :               tDict[xChar] = 1
+            tDict[xChar] = tDict.get(xChar, 0) + 1
         for i in sDict:
             if  sDict.get(i) != tDict.get(i) : return False
         return True
