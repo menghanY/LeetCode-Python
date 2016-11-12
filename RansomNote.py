dfc59b599c80083eddc7e75daec490716bc0cf6d
严梦寒 <yanmenghan@yanmenghandeMacBook-Pro.local>
2016-11-12 15:46:50 +0800

    完善文件夹结构

--- RansomNote.py
+++
@@ -1,4 +0,0 @@
-#Reference https://discuss.leetcode.com/topic/53889/many-different-ways-1-liners-2-liners-concise-4-liner-in-python-80ms/2
-class Solution(object):
-    def canConstruct(self, ransomNote, magazine):
-        return all(ransomNote.count(c)<=magazine.count(c) for c in set(ransomNote))
