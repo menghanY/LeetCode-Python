7a73a1bf1b2fde578da298dc9c666625c23cd23d
menghan <menghan_yan@foxmail.com>
2017-01-12 09:09:22 +0800

    binary tree level order traversal

---
+++ Tree/BinaryTreeLevelOrderTraversal.py
@@ -0,0 +1,19 @@
+from TreeNode import TreeNode
+class Solution(object):
+    def levelOrder(self, root):
+        res = []
+        if root :
+            q = [root]
+            i = 0
+            while i < len(q) :
+                level = []
+                for j in range(len(q) - i) :
+                    node = q[i]
+                    level.append(node.val)
+                    if node.left:
+                        q.append(node.left)
+                    if node.right:
+                        q.append(node.right)
+                    i += 1
+                res.append(level)
+        return res
